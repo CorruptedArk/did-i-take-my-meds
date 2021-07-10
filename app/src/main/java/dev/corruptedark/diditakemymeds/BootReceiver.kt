fec3d973be6be3d1e22431959cfbff8ed8b9d510
CorruptedArk <noahstandingford@gmail.com>
2021-07-09 22:53:01 -0400

    Start work towards notifications

---
+++ app/src/main/java/dev/corruptedark/diditakemymeds/BootReceiver.kt
@@ -0,0 +1,14 @@
+package dev.corruptedark.diditakemymeds
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+            // Set the alarm here.
+        }
+    }
+}
